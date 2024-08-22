bdaa7280d368d7548183edca55149ca11dd1904c
dima <dima@corporatefinanceinstitute.dev>
2024-08-22 19:37:26 +0300

    compile

---
+++ deploy/src/main/java/org/example/As400LFSourceFile.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class As400LFSourceFile extends As400PFSourceFile {
+    As400LFSourceFile(String localPath, String remotePath, String remoteLibrary, String remoteLibraryFile, String remoteMBRPath, String recordName) {
+        super(localPath, remotePath, remoteLibrary, remoteLibraryFile, remoteMBRPath, recordName);
+    }
+}
--- deploy/src/main/java/org/example/As400SpooledFileParser.java
+++ deploy/src/main/java/org/example/As400SpooledFileParser.java
@@ -7,7 +7,15 @@ import java.util.List;
  * Вытаскивает из контента spooled файла перечень ошибок в виде списка строк
  */
 public class As400SpooledFileParser {
-    public List<String> extractRPECompilationErrors(String text) {
+    public List<String> extractCompilationErrors(String text) {
+        if (text.contains("File attribute  . . . . . . . . . . . . . . . . . . :   Logical")) {
+            return extractLFCompilationErrors(text);
+        }
+
+        return extractRPECompilationErrors(text);
+    }
+
+    private List<String> extractRPECompilationErrors(String text) {
         List<String> errorMessages = new ArrayList<>();
         String[] lines = text.split("\n");
         boolean inErrorSection = false;
@@ -16,6 +24,26 @@ public class As400SpooledFileParser {
             if (line.contains("Msg id  Sv Number Message text")) {
                 inErrorSection = true;
             } else if (line.contains("E N D   O F   M E S S A G E   S U M M A R Y")) {
+                break;
+            } else if (inErrorSection) {
+                if (!line.trim().isEmpty()) {
+                    errorMessages.add(line.trim());
+                }
+            }
+        }
+
+        return errorMessages;
+    }
+
+    private List<String> extractLFCompilationErrors(String text) {
+        List<String> errorMessages = new ArrayList<>();
+        String[] lines = text.split("\n");
+        boolean inErrorSection = false;
+
+        for (String line : lines) {
+            if (line.contains("ID      Severity  Number")) {
+                inErrorSection = true;
+            } else if (line.contains("Message Summary")) {
                 break;
             } else if (inErrorSection) {
                 if (!line.trim().isEmpty()) {
